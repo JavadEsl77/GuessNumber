8549c7f2d228f12db0edc57b2e5658a29dea7de6
javadEsl <javades755@gmail.com>
2022-11-05 17:12:13 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+
+# def gamer(a, b):
+#     hads = random.randint(a, b)
+#     print(hads)
+#     javab = input('آیا درسته ؟')
+#     while javab != 'd':
+#         if javab == 'k':
+#             b = hads
+#         elif javab == 'b':
+#             a = hads
+#
+#         hads = random.randint(a, b)
+#         print(hads)
+#         javab = input('آیا درسته ؟')
+
+def gamer(a, b, j) -> int:
+    count = 0
+    hads = random.randint(a, b)
+    print(hads)
+
+    if hads > j:
+        javab = 'k'
+    elif hads < j:
+        javab = 'b'
+    else:
+        javab = 'd'
+
+    while javab != 'd':
+        if javab == 'k':
+            b = hads
+            count += 1
+        elif javab == 'b':
+            a = hads
+            count += 1
+
+        hads = random.randint(a, b)
+        print(hads)
+        if hads > j:
+            javab = 'k'
+        elif hads < j:
+            javab = 'b'
+        else:
+            javab = 'd'
+
+    return count +1
+
+
+a = int(input("a: "))
+b = int(input("B: "))
+j = int(input("j: "))
+print(gamer(a, b, j))
